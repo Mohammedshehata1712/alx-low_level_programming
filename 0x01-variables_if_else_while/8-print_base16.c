607ee577e67f8a0d485d9c268a1d90dea303d83e
mohammed <muhammad.ahmed614@hotmail.com>
2023-07-17 19:40:19 +0300

    s

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,12 +15,13 @@ int main(void)
 	while (digit <= 102) /*102; decimal rep of f*/
 	{
 		putchar(digit);
+
 		/* after 9 we jump till 96; '*/
 		if (digit == 57)
 			digit += 39;
 		digit++;
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
